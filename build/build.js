3e3eb54747d1c745ccf4e94af6d06f406cf86c0a
Jed Fox <git@jedfox.com>
2021-04-25 12:29:29 -0400

    add sf symbols

---
+++ build/build.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const path = require("path");
+const stripJsonComments = require("strip-json-comments");
+
+/** @type {{ [key: string]: string }} */
+const mapping = JSON.parse(
+  stripJsonComments(
+    fs.readFileSync(path.join(__dirname, "icon-mapping.jsonc"), "utf8")
+  )
+);
+
+const dealiased = Object.fromEntries(
+  Object.entries(mapping).map(([name, symbol]) => {
+    const match = /^\[(.+)\]$/.exec(symbol);
+    return [name, match ? mapping[match[1]] : symbol];
+  })
+);
+
+const theme = {
+  fonts: [
+    {
+      id: "sf-pro",
+      src: [
+        {
+          path: "/Library/Fonts/SF-Pro.ttf",
+          format: "woff",
+        },
+      ],
+      weight: "normal",
+      style: "normal",
+    },
+  ],
+  iconDefinitions: Object.fromEntries(
+    Object.entries(mapping).map(([name, symbol]) => {
+      const match = /^\[(.+)\]$/.exec(symbol);
+      return [name, { fontCharacter: match ? mapping[match[1]] : symbol }];
+    })
+  ),
+};
+
+fs.writeFileSync(
+  path.join(path.dirname(__dirname), "theme", "sf-symbols-theme.json"),
+  JSON.stringify(theme, null, 2)
+);
